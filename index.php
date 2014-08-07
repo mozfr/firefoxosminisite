c18954a8da0456083ffb76630a3b6f0db68b28e8
pascalc <pascal.chevrel@free.fr>
2014-08-07 23:55:47 +0200

    Initial home page

---
+++ index.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<html xml:lang="fr" lang="fr">
+<head>
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width,initial-scale=1" />
+  <title>Firefox OS en France</title>
+  <link rel="stylesheet" type="text/css" href="http://www.mozfr.org/custom/style/mozfr.css" />
+  <link rel="shortcut icon" type="image/x-icon" href="http://www.mozfr.org//favicon.ico" />
+
+</head>
+<body>
+    <div id="wrapper">
+        <header id="masthead">
+  <a href="http://mozfr.org" title="Retour au portail Mozilla Francophone" id="tabzilla">mozFR</a>
+  <nav id="nav-main" role="navigation">
+    <span class="toggle" role="button" aria-controls="nav-main-menu" tabindex="0">Menu</span>
+    <ul id="nav-main-menu">
+      <li class="first"><a href="./">Accueil</a></li>
+      <li><a href="http://forums.mozfr.org/">Forums</a></li>
+      <li><a href="http://www.mozfr.org/participer">Participer</a></li>
+      <li><a href="http://www.mozfr.org/manifesto">À propos</a></li>
+    </ul>
+  </nav>
+</header>
+<h1 id="site-title"><a href="./">Firefox OS en France</a></h1>
+<a href="http://www.ztefrance.com">
+  <div id="firefoxos-wrapper">
+    <span id="ffos-texte">Firefox&nbsp;OS arrive <br class="wide-br"> en France&nbsp;!</span>
+    <span id="ffos-learn-more" class="button">En savoir plus »</span>
+    <div id="firefoxos">
+      <div id="fox-front"></div>
+    </div>
+    <div id="phone"></div>
+  </div>
+</a>
+      <div id="content">
+
+      </div>
+
+    </div>
+  <div id="sidebar" class="aside">
+
+  </div>
+
+  <footer id="site-info" role="contentinfo">
+  <div class="wrap">
+    <p id="foot-logo">
+      <a class="top" href="#wrapper">Retour en haut</a>
+      <a class="logo" href="https://mozilla.org/fr/" rel="external">Mozilla</a>
+    </p>
+
+    <p id="nav-meta">
+      Ce site est maintenu par des bénévoles et n'est pas un site officiel de la
+      <a href="https://www.mozilla.org/fr/">fondation Mozilla</a>.
+    </p>
+  </div>
+</footer>
+
+  <script src="http://www.mozfr.org/custom/js/jquery-1.7.2.min.js" type="text/javascript"></script>
+  <script src="http://www.mozfr.org/mozfrtab/tabzilla.js"></script>
+  <script src="http://www.mozfr.org/custom/js/nav-main.js" type="text/javascript"></script>
+</body>
+</html>
+
