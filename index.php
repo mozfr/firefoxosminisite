9bacbd95878dca287d6f103aefa0e76127814e4f
Maton Anthony <maitre.negi@gmail.com>
2014-10-14 19:17:36 +0200

    Reprise et réorganisation du code Mozilla -- non achevé

---
+++ index.php
@@ -0,0 +1,14 @@
+<!doctype html>
+
+<html lang="fr">
+	<?php include('../includes/header.php'); ?>
+
+	<body>
+		<!-- Tabzilla général à tout les sites de la galaxie mozFR || DO NOT REMOVE -->
+		<a href="http://mozfr.org" title="Retour au portail Mozilla Francophone" id="tabzilla">mozFR</a>
+
+
+        <?php include('../includes/footer.php'); ?>
+
+	</body>
+</html>
