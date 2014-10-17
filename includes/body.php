5dcea1dfc3b04651b0d38a68d6f56effde609f29
Maton Anthony <maitre.negi@gmail.com>
2014-10-17 11:53:19 +0200

    Remplissage de certains fichiers

---
+++ includes/body.php
@@ -0,0 +1,76 @@
+<header class="mission">
+    <div class="container">
+        <h1><img class="js" src="FFxOS_Community_Bolt_WEB_5007x3240_72dpi.png" data-processed="true"> REGARDEZ L'AVENIR</h1>
+    </div>
+</header>
+
+<section class="promo-grid-wrapper">
+    <div class="promo-grid-inner">
+        <ul class="promo-grid  reveal">
+            <li id="promo-1" class="item promo-large-portrait open-standard" tabindex="0">
+                <h2 class="primary go"><img src="zteopenc-redim.png"> </h2>
+                <a class="panel-link" href="https://openstandard.mozilla.org/">
+                    <div class="secondary">
+                        <h3>ZTE OPEN C</h3>
+                        <p class="more">acheter</p>
+                    </div>
+                </a>
+            </li>
+            <li id="promo-2" class="item promo-small-landscape privacy">
+                <a class="panel-link" href="https://www-demo5.allizom.org/en-US/privacy/you/">
+                    <h2>Privacy => do not track </h2>
+                </a>
+            </li>
+            </li>
+            <li id="promo-5" class="item promo-small-landscape firefox-download" tabindex="0">
+                <div class="primary">
+                    <h2 class="go">AIDE</h2>
+                    <img src="Home%20of%20the%20Mozilla%20Project%20%E2%80%94%20Mozilla_fichiers/firefox-logo.png" alt="">
+                </div>
+                <div class="secondary">
+                    <h4>AIDE</h4>
+                    <h4>lien vers l'aide +faq</h4>
+                </div>
+            </li>
+            <li id="promo-6" class="item promo-large-landscape webmaker" tabindex="0">
+                <h2 class="primary go">Trouver des apps</h2>
+                <img src="marketplace_logo-wordmark_RGB.png">
+                <a class="panel-link" href="https://webmaker.org/">
+                    <div class="secondary">
+                        <h3>marketplace</h3>
+                        <p class="more">allez sur le marketplace</p>
+                    </div>
+                </a>
+            </li>
+            </li>
+            <li id="promo-8" class="item promo-small-landscape appmaker">
+                <a class="panel-link" href="https://webmaker.org/appmaker">
+                    <h2>Build an app <br>in seconds appmaker</h2>
+                </a>
+            </li>
+            <li id="promo-9" class="item promo-small-landscape volunteer">
+                <a class="panel-link" href="https://www-demo5.allizom.org/en-US/contribute/">
+                    <h2>lien vers RS ? <!-- lien vers les réseaux sociaux--> </h2>
+                </a>
+            </li>
+            <li id="promo-10" class="item promo-large-portrait android" tabindex="0">
+                <h2 class="primary go">RSS BLOG</h2>
+                <a class="panel-link" href=""> <!-- lien vers le dernier post-->
+                </a>
+            </li>
+            <li id="promo-11" class="item promo-large-landscape gear-store" tabindex="0">
+                <h2 class="primary go">twitts qui defile</h2>
+                <!-- voir exemple sur allizom-->
+                <a class="panel-link" href="https://gear.mozilla.org/">
+                    <div class="secondary">
+                        <h3>Merch with a mission. <br>Products with a purpose. <br>Goods that do good.</h3>
+                        <p class="more">Get your Mozilla gear</p>
+                    </div>
+                </a>
+            </li>
+
+
+
+    </div>
+</section>
+</div><!-- close #outer-wrapper -->
---
+++ includes/footer.php
@@ -0,0 +1,53 @@
+<footer id="colophon" class="">
+    <nav class="row">
+        <div class="col col-1">
+            <a class="logo" href="http://mozilla.org" rel="external">Mozilla</a>
+            <p class="license">Sauf mention contraire, le site est placé sous licence <a
+                    href="http://creativecommons.org/licenses/by-sa/3.0/">Creative Commons
+                    paternité partage à l’identique</a>.</p>
+        </div>
+        <div class="col col-2">
+            <p>Ce site est maintenu par des bénévoles et n'est pas un site officiel de la
+                <a href="http://www.mozilla.org/">fondation Mozilla.</a>
+        </div>
+        <div class="col col-3">
+            <ul class="links-social">
+                <li>
+                    Mozilla:
+                    <ul>
+                        <li><a href="https://twitter.com/mozilla">Twitter<span> (@mozilla)</span></a></li>
+                        <li><a href="https://www.facebook.com/mozilla">Facebook<span> (Mozilla)</span></a></li>
+                    </ul>
+                </li>
+                <li>
+                    Firefox:
+                    <ul>
+                        <li><a href="https://twitter.com/firefox">Twitter<span> (@firefox)</span></a></li>
+                        <li><a href="https://www.facebook.com/Firefox">Facebook<span> (Firefox)</span></a></li>
+                        <li><a href="https://www.youtube.com/firefoxchannel">YouTube<span> (firefoxchannel)</span></a></li>
+                    </ul>
+                </li>
+            </ul>
+            <div class="lang-switcher">
+            </div>
+        </div>
+    </nav>
+</footer>
+        <!-- JS nécessaire au fonctionnement du tabzilla en haut de page || DO NOT REMOVE -->
+		<script src="http://mozfr.org/mozfrtab/tabzilla.js" type="text/javascript"></script>
+		<script src="http://mozfr.org/custom/js/nav-main.js" type="text/javascript"></script>
+		<script src="http://mozfr.org/custom/js/jquery-1.7.2.min.js" type="text/javascript"></script>
+        <!-- Fin du JS lié au tabzilla -->
+
+        <script src="media/js/ga.js" async="" type="text/javascript"></script>
+        <!--[if IE 9]>
+        <script src="media/js/libs/matchMedia.js"></script>
+        <![endif]-->
+
+        <script src="media/js/mozorg-resp-min.js?build=28aaabb"></script>
+        <!--[if IE 9]>
+        <script src="media/js/home-2015-ie9-min.js?build=28aaabb"></script>
+        <![endif]-->
+        <script src="media/js/home-2015-min.js?build=28aaabb"></script>
+
+        <script src="media/js/home-2015-open-standard-min.js?build=28aaabb"></script>
