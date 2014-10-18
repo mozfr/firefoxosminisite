752e9289ea00242f074ec30a6478b5fa686ade10
TheoChevalier <theo.chevalier11@gmail.com>
2014-10-18 13:31:57 +0200

    Revert changes in TileTwitter

--- classes/TileTwitter.php
+++ classes/TileTwitter.php
@@ -14,14 +14,14 @@
  * utilisable avec le template prévu
  */
 
-require_once("../lib/twitteroauth/twitteroauth/twitteroauth.php"); // Path to twitteroauth library;
+require_once("lib/twitteroauth/twitteroauth/twitteroauth.php"); // Path to twitteroauth library;
 
 class TileTwitter {
-	// Donnée d'authentification à l'API Twitter
-	private $_consumerkey;
-	private $_consumersecret;
-	private $_accesstoken;
-	private $_accesstokensecret;
+    // Donnée d'authentification à l'API Twitter
+    private $_consumerkey;
+    private $_consumersecret;
+    private $_accesstoken;
+    private $_accesstokensecret;
 
     /**
      * Lit le fichier de configuration et s'authentifie auprès de Twitter
@@ -30,30 +30,34 @@ class TileTwitter {
      */
     public function __construct($pathToConf){
         if (!(file_exists($pathToConf) && is_readable($pathToConf))){
-			throw new Exception("TileTwitter.php : config file not found");
-		}else{
-			$config = parse_ini_file($pathToConf, false);
+            throw new Exception("TileTwitter.php : config file not found");
+        }else{
+            $config = parse_ini_file($pathToConf, false);
 
-			$this->_consumerkey = $config['consumerkey'];
-			$this->_consumersecret = $config['consumersecret'];
-			$this->_accesstoken = $config['accesstoken'];
-			$this->_accesstokensecret = $config['accesstokensecret'];
-		}
-	}
+            $this->_consumerkey = $config['consumerkey'];
+            $this->_consumersecret = $config['consumersecret'];
+            $this->_accesstoken = $config['accesstoken'];
+            $this->_accesstokensecret = $config['accesstokensecret'];
+        }
+    }
     public function getTweetWriteBy($idAccountToFollow,$numberOfTweet,$screenNameToFollow){
         $connection = new TwitterOAuth($this->_consumerkey,$this->_consumersecret, $this->_accesstoken,
             $this->_accesstokensecret);
         $tweets = $connection->get("statuses/user_timeline",array('count' => $numberOfTweet,'exclude_replies' => 1,
             'include_rts' => 0, 'user_id' => $idAccountToFollow, 'screen_name' => $screenNameToFollow));
-        $tweets = json_encode($tweets);
-        // DEBUG : Affiche le json obtenu
-        echo '<pre>';
-        $objJson = json_decode($tweets, TRUE); // TRUE me donne un array associatif, FALSE me donne un objet
-        print_r ($objJson);
-        echo '</pre>';
-        // DEBUG : Fin du débug
+        echo '<ul class="jcarousel">';
+        foreach ($tweets as $tweet) {
+            echo '<li id = "promo-8" class="item promo-small-landscape appmaker" >
+                        <a class="panel-link" href = "https://twitter.com/' . $screenNameToFollow . '" >
+                            <h2 > @' . $screenNameToFollow . ' </h2>
+                            <p > ' . $tweet['text'] . '</p >
+                        </a >
+                    </li >
+            ';
+        }
+        echo '</ul>';
     }
-	public function getFavoriteOf($idAccountToFollow,$numberOfTweet,$screenNameToFollow){
+    public function getFavoriteOf($idAccountToFollow,$numberOfTweet,$screenNameToFollow){
         $connection = new TwitterOAuth($this->_consumerkey,$this->_consumersecret, $this->_accesstoken,
             $this->_accesstokensecret);
         $tweets = $connection ->get("favorites/list",array('count' => $numberOfTweet, 'user_id' => $idAccountToFollow,
