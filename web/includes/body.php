997078c1e4cec59e788a705934b754e2d56be820
aurelienhubert <ahubert@mozilla.com>
2014-11-25 23:00:29 +0100

    URL update
    URL & description update for Mamie Fox, Foxprimez-vous, and Assistance.

--- web/includes/body.php
+++ web/includes/body.php
@@ -17,7 +17,7 @@
         <li id="promo-1" class="item promo-large-portrait mission" data-name="Application du jour" tabindex="0">
           <h2 class="primary go">L’appli du jour</h2>
           <img class="primary go" src="media/img/logo-marketplace.png" alt="Logo du Firefox Marketplace">
-          <a class="panel-link" href="http://blog.mozfr.org/post/2014/11/Aviary-de-nouveaux-outils-pour-une-edition-de-photos-qui-reste-simple" rel="external">
+          <a class="panel-link" href="http://blog.mozfr.org/post/Marketplace-Firefox-OS" rel="external">
             <div class="secondary">
               <h3>La communauté vous présente ses applications favorites sur Firefox&nbsp;OS.</h3>
               <p class="more">Découvrez l’application</p>
@@ -48,7 +48,7 @@
             <div id="download-button-desktop-release" class="download-button download-button-simple">
               <ul role="presentation" class="download-list">
                 <li>
-                  <a class="download-link" href="http://blog.mozfr.org/post/Page-Assistance-Firefox-OS" rel="external">
+                  <a class="download-link" href="http://blog.mozfr.org/pages/Assistance-de-Firefox-OS" rel="external">
                     <span class="download-content">
                       <span class="download-subtitle">Demandez de l'aide à la communauté</span>
                     </span>
@@ -90,7 +90,7 @@
           <a class="panel-link" href="http://blog.mozfr.org/post/2014/11/Apprendre-a-coder-sondage-et-article-foxprimezvous-" rel="external">
             <div class="secondary">
               <h3>Prenez la parole et la communauté vous répond&nbsp;!</h3>
-              <p class="more">Flore Allemandou + le Fox</p>
+              <p class="more">Apprendre à coder, qu'en pensez-vous ?</p>
             </div>
           </a>
         </li>
